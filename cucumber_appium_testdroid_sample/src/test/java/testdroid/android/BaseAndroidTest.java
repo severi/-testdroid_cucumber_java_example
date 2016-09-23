f2b8907b03b8c1c484803734b70ce840b08d4576
Severi Haverila <severi.haverila@bitbar.com>
2016-09-23 09:37:52 -0700

    server side implementation started

--- cucumber_appium_testdroid_sample/src/test/java/testdroid/android/BaseAndroidTest.java
+++ cucumber_appium_testdroid_sample/src/test/java/testdroid/android/BaseAndroidTest.java
@@ -21,6 +21,21 @@ public class BaseAndroidTest extends BaseTest{
          this.wd = new AndroidDriver(new URL(testdroidProperties.getProperty("testdroidServer")+"/wd/hub"), capabilities);
     }
 
+//    @Override
+//	protected void setDesiredCapabilities(String fileUUID) throws MalformedURLException {
+//		DesiredCapabilities capabilities = new DesiredCapabilities();
+//         capabilities.setCapability("platformName", testdroidProperties.getProperty("androidPlatfromName"));
+//         capabilities.setCapability("deviceName", testdroidProperties.getProperty("androidDeviceName"));
+//         capabilities.setCapability("testdroid_apiKey", testdroidProperties.getProperty("apiKey"));
+//         capabilities.setCapability("testdroid_target", testdroidProperties.getProperty("android_td_target"));
+//         capabilities.setCapability("testdroid_project", testdroidProperties.getProperty("android_td_project"));
+//         capabilities.setCapability("testdroid_testrun", testdroidProperties.getProperty("android_td_testRun"));
+//         capabilities.setCapability("testdroid_device", testdroidProperties.getProperty("android_td_device")); // Freemium device
+//         capabilities.setCapability("testdroid_app", fileUUID); //to use existing app using "latest" as fileUUID  
+//         System.out.println("Capabilities:" + capabilities.toString());
+//         this.capabilities = capabilities;
+//	}
+    
     @Override
 	protected void setDesiredCapabilities(String fileUUID) throws MalformedURLException {
 		DesiredCapabilities capabilities = new DesiredCapabilities();
